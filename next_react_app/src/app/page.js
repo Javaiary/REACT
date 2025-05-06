b4359dd4414ccf90df270e2c32ce5e0f2b396904
Javaiary <narrchoii@gmail.com>
2025-05-06 15:03:31 +0900

    NEXTJS

---
+++ next_react_app/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h2>Welcome</h2>
+    Hello NARI~!
+    <br/>
+    <img src="/shell.png" width="100px"></img>
+    </>
+  );
+}
+ 
